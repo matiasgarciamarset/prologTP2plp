0ab239902396c992fdfbba2c8a534f34af239829
matias garcia marset <matiasgarciamarset@MacBook-Pro-de-matias.local>
2015-10-25 16:35:18 -0300

    modificacion realizada por Martin.

--- bin/TP2.pl
+++ bin/TP2.pl
@@ -6,21 +6,38 @@
 
 % listaNats(+LInf,+LSup,?Nats), que unifica la lista Nats con los naturales en el rango [LInf, LSup], o una lista vacía si LSup < LInf.
 
-listaNats(_,_,_):- fail.
-
+% Unifica Nats con una lista vacia, un lista de un elemento, o
+% una lista recursiva dependiendo del signo de la diferencia entre X e Y.
+listaNats(X, Y, []) :- X > Y.
+listaNats(X, X, [X]).
+listaNats(X, Y, [X|T]) :- X < Y, Z is X + 1, listaNats(Z, Y, T).
 
 %%% Ejercicio 2
 
 % nPiezasDeCada(+Cant, +Tamaños, -Piezas), que instancia a Piezas con una lista que contiene 
 %  una cantidad Cant de cada tamaño en la lista Tamaños.
 
-nPiezasDeCada(_,_,_):- fail.
+% Unifica Piezas con una lista de una pirza por cada elemento de Tamaños.
+% Asume que Tamaños no tiene repetidos.
+nPiezasDeCada(_, [], []).
+nPiezasDeCada(N, [L|K], [H|T]) :- H = pieza(L, N), nPiezasDeCada(N, K, T).
 
 %%% Ejercicio 3
 
 % resumenPiezas(+SecPiezas, -Piezas), que permite instanciar Piezas con la lista de
 %  piezas incluidas en SecPiezas. 
-resumenPiezas(_, _):- fail.
+
+% Unifica Piezas con una lista ordenada de piezas con cada elemento igual a
+% un elemento de SecPiezas y la cantidad de veces que aparece.
+% Para hacer esto se aprovecha del predicado delete(+L1, @Elem, -L2) que
+% unifica L2 con L1 sin los elementos que matcheen con Elem.
+
+% Hay una manera mas linda de hacer esto?
+resumenPiezas([], []).
+resumenPiezas(S, P) :-
+	min_list(S, M), delete(S, M, D), length(S, A), length(D, B), L is A - B,
+	resumenPiezas(D, Q), nPiezasDeCada(L, [M], W),
+	append(W, Q, P).
 
 
 % ####################################
@@ -29,28 +46,44 @@ resumenPiezas(_, _):- fail.
 
 %%% Ejercicio 4
 
+% Predicado auxiliar:
+% sumList(?L, ?N), donde L es una lista de enteros positivos y N representa
+% su suma. Similar a sum_list, pero reversible.
+sumList([], 0).
+sumList([H|T], N) :- nonvar(N), between(1, N, H), Q is N - H, sumList(T, Q).
+sumList([H|T], N) :- ground([H|T]), sumList(T, X), N is X + H.
+sumList(L, N) :- var(N), between(1, inf, N), sumList(L, N).
+
 % generar(+Total,+Piezas,-Solución), donde Solución representa una lista de piezas
 %  cuyos valores suman Total. Aquí no se pide controlar que la cantidad de cada pieza
 %  esté acorde con la disponibilidad.
 
-generar(_,_,_):- fail.
-
+% Este puede generar varias soluciones iguales. Esto esta bien? Si no,
+% hay que modificar sumList para que solo devuelva listas ordenadas.
+generar(T, _, S) :- sumList(L, T), resumenPiezas(L, S).
 
 %%% Ejercicio 5 
 
+% Predicado auxiliar:
+% equalList(+L, +N, -S), donde S es una lista con N elementos iguales a L.
+equalList(_, 0, []).
+equalList(L, N, [L|T]) :- N > 0, M is N - 1, equalList(L, M, T).
+
+% Predicado auxiliar
+% piezasALista(+Piezas, -SecPiezas), donde L es una lista que contiene las piezas de P.
+% Es el inverso de resumenPiezas. Capaz este bueno juntar estas dos funciones.
+piezasALista([], []).
+piezasALista([pieza(R, C) | T], Q) :- equalList(R, C, A), append(A, B, Q), piezasALista(T, B).
+
 % cumpleLímite(+Piezas,+Solución) será verdadero cuando la cantidad de piezas utilizadas en Solución 
 %  no exceda las cantidades disponibles indicadas en Piezas
-
-cumpleLimite(_,_):- fail.
-
+cumpleLimite(Piezas, Solucion) :- piezasALista(Piezas, A), piezasALista(Solucion, B), ord_subset(B, A).
 
 %%% Ejercicio 6
 
 % construir1(+Total,+Piezas,-Solución), donde Solución representa una lista de piezas cuyos valores 
 %  suman Total y, además, las cantidades utilizadas de cada pieza no exceden los declarados en Piezas.
-
-construir1(_,_,_):- fail.
-
+construir1(T, P, S) :- generar(T, P, S), cumpleLimite(P, S).
 
 % ####################################
 % Enfoque dinámico
